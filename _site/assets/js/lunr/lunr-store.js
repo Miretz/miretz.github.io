50bfacfb20b11da59d89f4483336f587ded71917
miretz <miretz@gmail.com>
2019-03-05 21:08:53 +0100

    New article - On Meaningless work

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,12 +5,17 @@ var store = [{
         "url": "http://localhost:4000/analytics/2018/02/11/why-companies-need-text-mining.html",
         "teaser":"http://localhost:4000/assets/images/old-docs-teaser.jpeg"},{
         "title": "How to start new projects",
-        "excerpt":"In the current world of business, IT is tightly integrated into every process of the company. Each operation is usually supported, controlled or monitored using a specific information system. Payroll, accounting, internal and external communication, human resources, marketing, reporting and other activities rely heavily on IT systems operating correctly.All of...","categories": ["management"],
+        "excerpt":"In the current world of business, IT is tightly integrated into every process of the company. Each operation is usually supported, controlled or monitored using a specific information system. Payroll, accounting, internal and external communication, human resources, marketing, reporting and other activities rely heavily on IT systems operating correctly. All...","categories": ["management"],
         "tags": [],
         "url": "http://localhost:4000/management/2018/02/13/starting-new-projects.html",
         "teaser":"http://localhost:4000/assets/images/startup-photos-teaser.jpeg"},{
         "title": "How to manage software quality",
-        "excerpt":"At the heart of the software project lifecycle is the development phase. I have described how to get to this phase in my previous post on How to start new projects. In this article I’m going to dive deeper into the development phase with focus on how to prevent project...","categories": ["management"],
+        "excerpt":"At the heart of the software project lifecycle is the development phase. I have described how projects get to this phase in my previous post on How to start new projects. In this article I’m going to dive deeper into the development phase with focus on how to prevent project...","categories": ["management"],
         "tags": [],
         "url": "http://localhost:4000/management/2018/02/18/how-to-manage-software-quality.html",
-        "teaser":"http://localhost:4000/assets/images/code-quality-teaser.jpeg"}]
+        "teaser":"http://localhost:4000/assets/images/code-quality-teaser.jpeg"},{
+        "title": "On Meaningless work",
+        "excerpt":"On Literature I like reading books. Many people find them to be a good source of relaxation and enjoy stories which aim to kindle the childish imagination which is buried deep inside all of us. People might enjoy The Lord of the Rings, Harry Potter or The Hunger Games. Books...","categories": ["philosophy"],
+        "tags": [],
+        "url": "http://localhost:4000/philosophy/2018/02/18/on-meaningless-work.html",
+        "teaser":"http://localhost:4000/assets/images/meaningless-work-teaser.jpeg"}]
